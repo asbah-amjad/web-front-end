ce6d026357aadc87a815be9589a91d946d74747f
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-04 20:32:50 +0200

    Exercise 1.2 done

--- round01/01-courseinfo/src/components/App.js
+++ round01/01-courseinfo/src/components/App.js
@@ -13,10 +13,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercise}</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
+      <Part name='Fundamentals of React' exercise={10}/>
+      <Part name='Using props to pass data' exercise={7}/>
+      <Part name='State of a component' exercise={14}/>
     </div>
   )
 }
@@ -32,19 +42,14 @@ const Total = (props) => {
 export const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
