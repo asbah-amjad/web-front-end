9ab89abc0c88545e699949357f2f9e15eead620a
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-06 15:55:06 +0200

    Exercise 1.6 done

--- round01/02-unicafe/src/components/App.js
+++ round01/02-unicafe/src/components/App.js
@@ -4,16 +4,51 @@ import { useState } from 'react';
 // ------------------------------------------------------------ //
 // ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
 // ------------------------------------------------------------ //
-export const commitSHA = 'commit-sha-in-here';
+export const commitSHA = '1ca2c5f';
 // ------------------------------------------------------------ //
 
+const Display = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
+const Values = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.value}</p>
+    </div>
+  )
+}
 
 export const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  
+  const goodClickHandler = () => setGood(good + 1)
+  const neutralClickHandler = () => setNeutral(neutral + 1)
+  const badClickHandler = () => setBad(bad + 1)
+
   return (
-    <div>unicafe</div>
+    <div>
+      <Display name="give feedback" />
+      <Button handleClick={goodClickHandler} text="good" />
+      <Button handleClick={neutralClickHandler} text="neutral" />
+      <Button handleClick={badClickHandler} text="bad" />
+      <Display name="statistics"/>
+      <Values name="good" value={good}/>
+      <Values name="neutral" value={neutral}/>
+      <Values name="bad" value={bad}/>
+    </div>
   )
 }
