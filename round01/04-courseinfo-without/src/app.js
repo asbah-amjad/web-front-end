757134c47b1943c71bd55a4eb306af3d93fda281
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-09 10:20:20 +0200

    without jsx done

--- round01/04-courseinfo-without/src/app.js
+++ round01/04-courseinfo-without/src/app.js
@@ -4,15 +4,35 @@
 // ------------------------------------------------------------ //
 // ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
 // ------------------------------------------------------------ //
-const commitSHA = 'commit-sha-in-here';   //
+const commitSHA = '1ca2c5f';   
 // ------------------------------------------------------------ //
 
-const Header = props => null;
+const e = React.createElement;
+
+const Header = props => {
+    return e( 'div', null,
+        e( 'h1', null, props.course.name)
+    );
+};
 
 const Part = props => null;
 
-const Content = props => null;
+const Content = props => {
+    return e("div", null,
+     e("p", null, props.course.parts[0].name, " ", props.course.parts[0].exercises), 
+     e("p", null, props.course.parts[1].name, " ", props.course.parts[1].exercises), 
+     e("p", null, props.course.parts[2].name, " ", props.course.parts[2].exercises));
+  };
 
-const Total = props => null;
+  const Total = props => {
+    return e("div", null, 
+    e("p", null, 
+    props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises));
+  };
 
-const App = ({ course }) => null;
+const App = ({ course }) => {
+    return e( 'div', null,
+    e( Header, {course: course}),
+    e( Content, {course: course}),
+    e( Total, {course: course}));
+};
