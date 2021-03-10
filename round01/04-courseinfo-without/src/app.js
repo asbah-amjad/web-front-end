45cf21dd4c764cddbfd622bab3503cd058e359b7
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-10 14:08:02 +0200

    without react part not working

--- round01/04-courseinfo-without/src/app.js
+++ round01/04-courseinfo-without/src/app.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 // ------------------------------------------------------------ //
@@ -7,32 +6,32 @@
 const commitSHA = '1ca2c5f';   
 // ------------------------------------------------------------ //
 
-const e = React.createElement;
 
 const Header = props => {
-    return e( 'div', null,
-        e( 'h1', null, props.course.name)
+    return createElement( 'div', null,
+    createElement( 'h1', null, props.course.name)
     );
 };
 
 const Part = props => null;
 
 const Content = props => {
-    return e("div", null,
-     e("p", null, props.course.parts[0].name, " ", props.course.parts[0].exercises), 
-     e("p", null, props.course.parts[1].name, " ", props.course.parts[1].exercises), 
-     e("p", null, props.course.parts[2].name, " ", props.course.parts[2].exercises));
+    return createElement("div", null,
+    createElement("p", null, props.course.parts[0].name, " ", props.course.parts[0].exercises), 
+    createElement("p", null, props.course.parts[1].name, " ", props.course.parts[1].exercises), 
+    createElement("p", null, props.course.parts[2].name, " ", props.course.parts[2].exercises));
   };
 
   const Total = props => {
-    return e("div", null, 
-    e("p", null, 
+    return createElement("div", null, 
+    createElement("p", null, 
     props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises));
   };
 
 const App = ({ course }) => {
-    return e( 'div', null,
-    e( Header, {course: course}),
-    e( Content, {course: course}),
-    e( Total, {course: course}));
+  
+    return createElement( 'div', null,
+    createElement( Header, {course: course}),
+    createElement( Content, {course: course}),
+    createElement( Total, {course: course}));
 };
--- round01/04-courseinfo-without/src/myact.js
+++ round01/04-courseinfo-without/src/myact.js
@@ -1,9 +1,23 @@
 
+function isFunction(functionToCheck) {
+  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+ }
+
 const Myact = {
 
   createElement: function (tag, props, ...childNodes) {
 
+    if(typeof tag === 'string'){
 
+      const newElement = document.createElement(tag);
+      for(let i=0; i<childNodes.length; i++){
+        newElement.innerHTML = childNodes[i].text;
+        root.append(newElement);
+      }
+    }
+    if(isFunction(tag) === true){
+      return tag(props);
+    }
   }
   
 }
@@ -11,8 +25,10 @@ const Myact = {
 const MyactDOM = {
 
   render: function (element, parent) {
-
+    parent.remove();
     
+    parent.append(element);
+             
   }
 
 }
