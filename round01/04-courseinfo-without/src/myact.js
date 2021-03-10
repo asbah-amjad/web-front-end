073086706d456928ccb32bcb62f0dd6d1a1fa014
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-10 19:11:20 +0200

    jsx|react done

--- round01/04-courseinfo-without/src/myact.js
+++ round01/04-courseinfo-without/src/myact.js
@@ -1,23 +1,23 @@
 
-function isFunction(functionToCheck) {
-  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
- }
-
 const Myact = {
 
   createElement: function (tag, props, ...childNodes) {
 
     if(typeof tag === 'string'){
-
-      const newElement = document.createElement(tag);
+      const newElement = document.createElement(tag); 
       for(let i=0; i<childNodes.length; i++){
-        newElement.innerHTML = childNodes[i].text;
-        root.append(newElement);
+        newElement.append(childNodes[i]);
       }
+      return newElement;    
     }
-    if(isFunction(tag) === true){
-      return tag(props);
+    if(typeof tag === 'function'){
+      const newElement = tag(props);
+      for(let i=0; i<childNodes.length; i++){
+        newElement.append(childNodes[i]);
+      }
+      return newElement;
     }
+
   }
   
 }
@@ -25,8 +25,10 @@ const Myact = {
 const MyactDOM = {
 
   render: function (element, parent) {
-    parent.remove();
-    
+
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }  
     parent.append(element);
              
   }
