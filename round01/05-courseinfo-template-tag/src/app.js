bf6c097eb01f4771c9fe4e563f11a8864a46ba42
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-10 22:32:19 +0200

    ex 5 done

--- round01/05-courseinfo-template-tag/src/app.js
+++ round01/05-courseinfo-template-tag/src/app.js
@@ -2,12 +2,23 @@
 // ------------------------------------------------------------ //
 // ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
 // ------------------------------------------------------------ //
-const commitSHA = 'commit-sha-in-here';   //
+const commitSHA = '1ca2c5f';   //
 // ------------------------------------------------------------ //
 
 const App = ({data, template}) => {
 
+  let templateClone = template.content.cloneNode(true)
+  templateClone.querySelector('.header').textContent = data.name
 
-  return document.createTextNode("hello courseinfo template tag");
+  let content = templateClone.querySelector('.content').querySelectorAll('p')
+  let total = 0
+  for(let i = 0; i < content.length; i++) {
+    content[i].textContent = data.parts[i].name + ' ' + data.parts[i].exercises
+    total += data.parts[i].exercises
+  }
+
+  templateClone.querySelector('.total').textContent = total
+
+  return templateClone
 
 };
