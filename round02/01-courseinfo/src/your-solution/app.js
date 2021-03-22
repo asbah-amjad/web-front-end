96af23f241e9319f7da635ab168048256cf2e7e8
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-22 14:34:26 +0200

    2.1 done

--- round02/01-courseinfo/src/your-solution/app.js
+++ round02/01-courseinfo/src/your-solution/app.js
@@ -2,15 +2,57 @@
 // ------------------------------------------------------------ //
 // ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
 // ------------------------------------------------------------ //
-export const commitSHA = 'commit-sha-in-here';
+export const commitSHA = '1ca2c5f';
 // ------------------------------------------------------------ //
 
 
-export const  App = () => {
+const Header = props =>
+  <h1>
+    {props.course}
+  </h1>
+
+const Part = ({ parts }) => {
+  return (
+    <p>{parts.name} {parts.exercises}</p>
+  )
+}
+
+const Course = ({ course }) => {
   return (
     <div>
-        course information
+      <Header course={course.name} />
+        {course.parts.map(parts => 
+          <Part key={parts.id} parts={parts} />
+        )}   
     </div>
-  );
+  )
+}
+
+export const App = () => {
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+
 }
 
