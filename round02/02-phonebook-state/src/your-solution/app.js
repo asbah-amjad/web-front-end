e23a888d9d639d866f646ed5be07fceb968d8d6a
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-24 10:43:36 +0200

    2.9 done

--- round02/02-phonebook-state/src/your-solution/app.js
+++ round02/02-phonebook-state/src/your-solution/app.js
@@ -11,11 +11,18 @@ const Persons = ({ persons }) => {
   )
 }
 
+const PersonsInfo = ({ persons }) => {
+  const personsList = persons.map((persons => 
+    <Persons key={persons.name} persons={persons} /> ));
+
+  return <div>{personsList}</div>  
+}
+
 export const App = () => {
   const [ persons, setPersons ] = useState([{ name: 'Arto Hellas', number: '040-123456' }]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-  
+  const [ search, setSearch ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -38,34 +45,37 @@ export const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+
+  const dynamicSearch = () => {
+    return persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>filter shown with 
-        <input />
+        <input value={search} onChange={handleSearch} />
       </div>
       <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input 
-            value={newName}
-            onChange={handleNameChange}
-                />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input 
-            value={newNumber}
-            onChange={handleNumberChange}
-                />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(persons => 
-            <Persons key={persons.name} persons={persons} />
-        )}
+        <PersonsInfo persons={dynamicSearch()}/>
+       
+        
     </div>
   )
 }
