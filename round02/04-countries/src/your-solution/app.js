6443dffc8739294aaea6c4f98a13b55b8bc96422
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-03-24 21:57:37 +0200

    02-04

--- round02/04-countries/src/your-solution/app.js
+++ round02/04-countries/src/your-solution/app.js
@@ -1,16 +1,43 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { CountryDetails, Filter} from './components';
 // ------------------------------------------------------------ //
 // ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
 // ------------------------------------------------------------ //
-export const commitSHA = 'commit-sha-in-here';
+export const commitSHA = '1ca2c5f';
 // ------------------------------------------------------------ //
 
-
 export const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, SetSearchTerm] = useState("");
+  const handleFilterChange = (event) => SetSearchTerm(event.target.value);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.warn(error));
+  }, []);
+
+  const resultCountries = () =>
+    countries.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+  const handleShowClick = (city) => {
+    SetSearchTerm(city);
+  };
+
   return (
-    <div>
-      countries
-    </div>
+    <>
+      <Filter onChange={handleFilterChange} value={searchTerm} />
+      {searchTerm && (
+        <CountryDetails
+          onButtonClick={handleShowClick}
+          countries={resultCountries()}
+        />
+      )}
+    </>
   );
 }
 
