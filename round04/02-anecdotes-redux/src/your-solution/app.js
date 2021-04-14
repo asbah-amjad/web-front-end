2649d165276afd1f1c5603a57b30f7d89c8ba0ec
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-04-14 15:15:06 +0300

    till 6.8 done

--- round04/02-anecdotes-redux/src/your-solution/app.js
+++ round04/02-anecdotes-redux/src/your-solution/app.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { createStore } from 'redux'
-import { useSelector, useDispatch } from 'react-redux'
 import reducer from './anecdoteReducer'
-import { votesOf, createNew } from './anecdoteReducer'
+import { AnecdoteForm, AnecdoteList } from './components'
 
 export const store = createStore(reducer)
 
@@ -10,44 +9,12 @@ export const store = createStore(reducer)
 export const commitSHA = '-commit-sha-in-here-';
 
 export const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id, votes) => {
-    console.log('vote', id)
-    dispatch(votesOf(id, votes))
-  }
-
-  const addVote = (event) => {
-    event.preventDefault()
-    const content = event.target.vote.value
-    event.target.vote.value = ''
-    dispatch(createNew(content))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {[...anecdotes]
-        .sort((a, b) =>
-          (b.votes >= 0 ? b.votes : 0) - (a.votes >= 0 ? a.votes : 0)
-        )
-        .map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id, anecdote.votes)}>vote</button>
-            </div>
-          </div>
-        )}
-      <h2>create new</h2>
-      <form onSubmit={addVote}>
-        <input name="vote" />
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
--- round04/02-anecdotes-redux/src/your-solution/components.js
+++ round04/02-anecdotes-redux/src/your-solution/components.js
@@ -1,5 +1,58 @@
 
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { votesOf, createNew } from './anecdoteReducer'
+
+export const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addVote = (event) => {
+    event.preventDefault()
+    const content = event.target.vote.value
+    event.target.vote.value = ''
+    dispatch(createNew(content))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addVote}>
+        <input name="vote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id, votes) => {
+    console.log('vote', id)
+    dispatch(votesOf(id, votes))
+  }
+
+  return (
+    <div>
+      {[...anecdotes]
+        .sort((a, b) =>
+          (b.votes >= 0 ? b.votes : 0) - (a.votes >= 0 ? a.votes : 0)
+        )
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id, anecdote.votes)}>vote</button>
+            </div>
+          </div>
+        )}
+    </div>
+  )
+}
 
 export const Notification = () => {
   const style = {
@@ -13,5 +66,7 @@ export const Notification = () => {
     </div>
   )
 }
+
+
 
 
