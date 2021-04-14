844760fd276ae789528ef7cb69e8df30956e6859
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-04-14 12:28:28 +0300

    6.3 done

---
+++ round04/02-anecdotes-redux/src/your-solution/anecdoteReducer.js
@@ -0,0 +1,56 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch (action.type) {
+    case 'VOTE':
+      return state.map((item, index) => {
+        // Find the item with the matching id
+        if (item.id === action.data.id) {
+          // Return a new object
+          return {
+            ...item,  // copy the existing item
+            votes: action.data.votes + 1  // replace the vote
+          }
+        }
+
+        // Leave every other item unchanged
+        return item;
+      });
+    default:
+      return state
+  }
+}
+
+export const votesOf = (id, votes) => {
+  return {
+    type: 'VOTE',
+    data: {
+      id,
+      votes,
+    }
+  }
+}
+
+export default reducer
--- round04/02-anecdotes-redux/src/your-solution/app.js
+++ round04/02-anecdotes-redux/src/your-solution/app.js
@@ -1,2 +1,42 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { useSelector, useDispatch } from 'react-redux'
+import reducer from './anecdoteReducer'
+import { votesOf } from './anecdoteReducer'
 
-// replace this with the starter
+export const store = createStore(reducer)
+
+// ** enter commit sha of your repository in here **
+export const commitSHA = '-commit-sha-in-here-';
+
+export const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id, votes) => {
+    console.log('vote', id)
+    dispatch(votesOf(id, votes))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id, anecdote.votes)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
