08d6231aae7a342e99de6d88da47a5e482d64e89
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-05-04 03:01:36 +0300

    05-02 done

--- round05/02-more-hooks/ultimate-hooks/src/your-solution/app-ultimate.js
+++ round05/02-more-hooks/ultimate-hooks/src/your-solution/app-ultimate.js
@@ -1,9 +1,9 @@
-  
+
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 // ** enter commit sha of your repository in here **
-export const commitSHA = '-commit-sha-in-here-';
+export const commitSHA = '1ca2c5f';
 
 const useField = (type) => {
   const [value, setValue] = useState('')
@@ -21,11 +21,17 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
+  const url = baseUrl
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl).then(response => {
+      setResources(response.data)
+    })
+  }, [url])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(url, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
@@ -49,10 +55,10 @@ export const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -66,7 +72,7 @@ export const App = () => {
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
@@ -74,4 +80,3 @@ export const App = () => {
     </div>
   )
 }
-
