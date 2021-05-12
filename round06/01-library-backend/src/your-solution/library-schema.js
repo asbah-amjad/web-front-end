514e2af4fc87f49887bfc96940f8c48f9753b9c7
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-05-12 12:07:48 +0300

    8.1 done

--- round06/01-library-backend/src/your-solution/library-schema.js
+++ round06/01-library-backend/src/your-solution/library-schema.js
@@ -3,7 +3,21 @@ const { gql } = require('apollo-server')
 
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    born: String!
+    id: ID!
+  }
+  type Book {
+    title: String!
+    published: String!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
