42cd02a8644a3f9ad0b312ac055a4dc58c4eaf6c
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-05-30 07:36:33 +0300

    07-01 done

--- round07/01-body-mass-index/src/your-solution/bmi-web.ts
+++ round07/01-body-mass-index/src/your-solution/bmi-web.ts
@@ -4,4 +4,27 @@ import { calculateBmi } from './bmi-calculator';
 
 const app = express();
 
+app.get('/bmi', (req, res) => {
 
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+    if(!height || !weight ) {
+        res.json({'error': 'malformatted parameters'})
+    }
+    if(!isNaN(height) && !isNaN(weight)){
+        const bmi = calculateBmi(height, weight);
+        res.json({weight: weight,
+            height: height,
+            bmi: bmi});
+    }
+    else{
+        res.json({'error': 'malformatted parameters'})
+    }   
+    
+});
+  
+const PORT = 3003;
+  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
