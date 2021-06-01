6dccbd124691f45c191e7522e9f75bddf3924aa2
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-06-01 07:37:56 +0300

    9.14 done

--- round07/03-course-with-types/src/your-solution/app.tsx
+++ round07/03-course-with-types/src/your-solution/app.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-//import { Header, Content, Total } from './components';
+import { Header, Content, Total } from './components';
 
 
 // ** enter commit sha of your repository in here **
-export const commitSHA = '-commit-sha-in-here-';
+export const commitSHA = '1ca2c5f';
 
 export const App = () => {
   const courseName = "Half Stack application development";
@@ -22,7 +22,7 @@ export const App = () => {
       exerciseCount: 14
     }
   ];
-
+/*
   return (
 
     <div>
@@ -42,14 +42,14 @@ export const App = () => {
       </p>
     </div>
   );
-
-  // return (
-  //   <div>
-  //     <Header name={courseName} />
-  //     <Content ... />
-  //     <Total ... />
-  //   </div>
-  // )
+*/
+   return (
+     <div>
+       <Header name={courseName} />
+      <Content courseParts={courseParts} />  
+      <Total courseParts={courseParts}/>    
+    </div>
+   )
 
 };
 
--- round07/03-course-with-types/src/your-solution/components.tsx
+++ round07/03-course-with-types/src/your-solution/components.tsx
@@ -2,29 +2,46 @@
 import React from 'react';
 
 
-export const Header = () => {
+export const Header = ({name}: {name: string}) => {
 
   return (
-    <div>Header</div>
+    <div>
+      <h1>{name}</h1>
+    </div>
   );
 
 };
 
+interface Course{
+  name: string,
+  exerciseCount: number
+}
+interface CourseProps {
+  courseParts: Course[];
+}
 
-export const Content = () => {
+export const Content: React.FC<CourseProps> = ({courseParts}: CourseProps) => {
 
   return (
-    <div>Content</div>
+    <div>
+      {courseParts.map(c=> (
+        <p key={c.name}> {c.name} {c.exerciseCount}</p>
+      ))}
+    </div>
   );
-
 };
 
 
 
-export const Total = () => {
+export const Total: React.FC<CourseProps> = ({courseParts}: CourseProps) => {
 
   return (
-    <div>Total</div>
+    <div>Total
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
   );
 
 };
